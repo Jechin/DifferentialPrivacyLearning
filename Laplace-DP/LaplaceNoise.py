298a06df6e35a8a72aa8979675fa32cbf80a18db
Jechin <60766652+Jechin@users.noreply.github.com>
2021-10-31 15:54:11 +0800

    Add Laplace Code.

---
+++ Laplace-DP/LaplaceNoise.py
@@ -0,0 +1,12 @@
+'''
+Description: Laplace-DP
+Autor: Jechin
+Date: 2021-10-31 10:07:20
+LastEditors: Jechin
+LastEditTime: 2021-10-31 15:20:15
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
